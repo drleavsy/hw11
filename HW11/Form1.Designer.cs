6e05d9bfd915083b5cafcb0b58828fba83d01705
Dmytro Stoliar <drleavsy@gmail.com>
2016-12-24 19:35:02 +0200

    stack push implemented along with GUI

---
+++ HW11/Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace HW11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.LinkedListStack = new System.Windows.Forms.Button();
+            this.DynamicArrayStack = new System.Windows.Forms.Button();
+            this.regular_array = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.EXIT = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.LinkedListStack);
+            this.groupBox1.Controls.Add(this.DynamicArrayStack);
+            this.groupBox1.Controls.Add(this.regular_array);
+            this.groupBox1.Location = new System.Drawing.Point(12, 43);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(180, 111);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Stack - Pushing";
+            // 
+            // label3
+            // 
+            this.label3.AccessibleDescription = "Linked-List Stack Label";
+            this.label3.AccessibleName = "LinkedListStackLabel";
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(88, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(88, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(88, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "0";
+            // 
+            // LinkedListStack
+            // 
+            this.LinkedListStack.Location = new System.Drawing.Point(7, 79);
+            this.LinkedListStack.Name = "LinkedListStack";
+            this.LinkedListStack.Size = new System.Drawing.Size(75, 23);
+            this.LinkedListStack.TabIndex = 2;
+            this.LinkedListStack.Text = "Linked-list";
+            this.LinkedListStack.Click += new System.EventHandler(this.LinkedLitStack_Click);
+            // 
+            // DynamicArrayStack
+            // 
+            this.DynamicArrayStack.AccessibleDescription = "Dynamic Array Stack";
+            this.DynamicArrayStack.AccessibleName = "DynamicArrayStack";
+            this.DynamicArrayStack.Location = new System.Drawing.Point(6, 49);
+            this.DynamicArrayStack.Name = "DynamicArrayStack";
+            this.DynamicArrayStack.Size = new System.Drawing.Size(75, 23);
+            this.DynamicArrayStack.TabIndex = 1;
+            this.DynamicArrayStack.Text = "Dynamic Array";
+            this.DynamicArrayStack.UseVisualStyleBackColor = true;
+            this.DynamicArrayStack.Click += new System.EventHandler(this.DynamicArrayStack_click);
+            // 
+            // regular_array
+            // 
+            this.regular_array.AccessibleDescription = "regular_array";
+            this.regular_array.AccessibleName = "regular_array";
+            this.regular_array.Location = new System.Drawing.Point(7, 20);
+            this.regular_array.Name = "regular_array";
+            this.regular_array.Size = new System.Drawing.Size(75, 23);
+            this.regular_array.TabIndex = 0;
+            this.regular_array.Text = "Array-based";
+            this.regular_array.UseVisualStyleBackColor = true;
+            this.regular_array.Click += new System.EventHandler(this.regularArray_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Controls.Add(this.button5);
+            this.groupBox2.Controls.Add(this.button6);
+            this.groupBox2.Location = new System.Drawing.Point(209, 43);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(177, 111);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Queue";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(97, 84);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "label6";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(97, 54);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "label5";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(97, 25);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "label4";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(7, 79);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "Linked-list";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.MouseLeave += new System.EventHandler(this.LinkedLitStackButton_Leave);
+            this.button4.MouseHover += new System.EventHandler(this.LinkedLitStackButton_Hover);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(6, 49);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 1;
+            this.button5.Text = "Dynamic Array";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(7, 20);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 0;
+            this.button6.Text = "Array-based";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // EXIT
+            // 
+            this.EXIT.BackColor = System.Drawing.SystemColors.Control;
+            this.EXIT.Font = new System.Drawing.Font("Cambria", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EXIT.Location = new System.Drawing.Point(287, 217);
+            this.EXIT.Name = "EXIT";
+            this.EXIT.Size = new System.Drawing.Size(75, 23);
+            this.EXIT.TabIndex = 4;
+            this.EXIT.Text = "EXIT";
+            this.EXIT.UseVisualStyleBackColor = false;
+            this.EXIT.Click += new System.EventHandler(this.EXIT_Click);
+            this.EXIT.MouseLeave += new System.EventHandler(this.Exit_Leave);
+            this.EXIT.MouseHover += new System.EventHandler(this.Exit_Hover);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(9, 9);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(388, 18);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "Press any button in order to measure implementation time";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(398, 262);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.EXIT);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "WinForms Stack&Queue";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button LinkedListStack;
+        private System.Windows.Forms.Button DynamicArrayStack;
+        private System.Windows.Forms.Button regular_array;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button EXIT;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ HW11/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Drawing;
+
+namespace HW11
+{
+    public partial class Form1 : Form
+    {
+        private string LabelNew;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string LabelStackLink
+        {
+            get { return LabelNew; }
+            private set { LabelNew = value; }
+        }
+
+        private void DynamicArrayStack_click(object sender, EventArgs e)
+        {
+            int bufferSize = 10000;
+            Stopwatch watch = new Stopwatch();
+            Random random = new Random();
+
+            //HW9A.DynamicStack<int> StackInst = new HW9A.DynamicStack<int>(bufferSize);
+            HW9A.DynamicStack<int> StackInst2 = new HW9A.DynamicStack<int>(bufferSize);
+            watch.Start();
+            if (!StackInst2.IsFull())
+            {
+                for (int i = 0; i < 10000; i++)
+                {
+                    StackInst2.Push(999999/*random.Next()*/);
+                }
+            }
+            watch.Stop();
+
+            long nanosecPerTick = (watch.ElapsedTicks * 1000L * 1000L) / Stopwatch.Frequency;
+            LabelStackLink = String.Format("{0} micro sec", nanosecPerTick);
+            Console.WriteLine(String.Format("{0} ms", watch.ElapsedMilliseconds));
+            this.label2.Text = this.LabelStackLink;
+            this.label2.Refresh();
+        }
+
+        private void regularArray_Click(object sender, EventArgs e)
+        {
+            int[] bufferSize = new int[10000] ;
+            Stopwatch watch = new Stopwatch();
+            Random random = new Random();
+
+            HW7.MyStack<int> StackInst3 = new HW7.MyStack<int>(bufferSize);
+            watch.Start();
+            if (!StackInst3.IsFull())
+            {
+                for (int i = 0; i < 10000; i++)
+                {
+                    StackInst3.Push(999999/*random.Next()*/);
+                }
+            }
+            watch.Stop();
+
+            long nanosecPerTick = (watch.ElapsedTicks * 1000L * 1000L) / Stopwatch.Frequency;
+            LabelStackLink = String.Format("{0} micro sec", nanosecPerTick);
+            Console.WriteLine(String.Format("{0} ms", watch.ElapsedMilliseconds));
+            this.label1.Text = this.LabelStackLink;
+            this.label1.Refresh();
+        }
+
+        private void LinkedLitStack_Click(object sender, EventArgs e)
+        {
+            int bufferSize = 10000;
+            Stopwatch watch = new Stopwatch();
+            Random random = new Random();
+
+            HW9B.DynamicStack<int> StackInst = new HW9B.DynamicStack<int>(bufferSize);
+
+            watch.Start();
+            if (!StackInst.IsFull())
+            {
+                for (int i = 0; i < 10000; i++)
+                {
+                    StackInst.Push(999999/*random.Next()*/);
+                }
+            }
+            watch.Stop();
+
+            long nanosecPerTick = (watch.ElapsedTicks * 1000L * 1000L) / Stopwatch.Frequency;
+            LabelStackLink = String.Format("{0} micro sec", nanosecPerTick);
+            Console.WriteLine(String.Format("{0} ms", watch.ElapsedMilliseconds));
+            this.label3.Text = this.LabelStackLink;
+            this.label3.Refresh();
+        }
+
+        private void LinkedLitStackButton_Hover(object sender, EventArgs e)
+        {
+            this.LinkedListStack.BackColor = Color.LightGray;
+        }
+        private void LinkedLitStackButton_Leave(object sender, EventArgs e)
+        {
+            this.LinkedListStack.BackColor = Color.WhiteSmoke;
+        }
+
+        private void Exit_Hover(object sender, EventArgs e)
+        {
+            this.EXIT.BackColor = Color.LightGray;
+        }
+        private void Exit_Leave(object sender, EventArgs e)
+        {
+            this.EXIT.BackColor = Color.WhiteSmoke;
+        }
+
+        private void EXIT_Click(object sender, EventArgs e)
+        {
+            Dispose(true);
+            //return;
+        }
+    }
+}
